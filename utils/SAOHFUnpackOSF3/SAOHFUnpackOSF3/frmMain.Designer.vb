c366137d19acd578b6a31067d0e01f85fd42a2ee
Kirby0Louise <56141999+Kirby0Louise@users.noreply.github.com>
2025-04-16 19:51:58 -0400

    Add SAOHFUnpackOFS3

---
+++ utils/SAOHFUnpackOSF3/SAOHFUnpackOSF3/frmMain.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTargetPath = New System.Windows.Forms.TextBox()
+        Me.lblTargetPath = New System.Windows.Forms.Label()
+        Me.lblOutputPath = New System.Windows.Forms.Label()
+        Me.txtOutputPath = New System.Windows.Forms.TextBox()
+        Me.cmdUnpack = New System.Windows.Forms.Button()
+        Me.txtLog = New System.Windows.Forms.TextBox()
+        Me.cmdSelectFile = New System.Windows.Forms.Button()
+        Me.cmdSelectDir = New System.Windows.Forms.Button()
+        Me.cmdNoFileNameUnpack = New System.Windows.Forms.Button()
+        Me.cmdQuickScript = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtTargetPath
+        '
+        Me.txtTargetPath.Location = New System.Drawing.Point(12, 25)
+        Me.txtTargetPath.Name = "txtTargetPath"
+        Me.txtTargetPath.Size = New System.Drawing.Size(776, 20)
+        Me.txtTargetPath.TabIndex = 0
+        '
+        'lblTargetPath
+        '
+        Me.lblTargetPath.AutoSize = True
+        Me.lblTargetPath.Location = New System.Drawing.Point(12, 9)
+        Me.lblTargetPath.Name = "lblTargetPath"
+        Me.lblTargetPath.Size = New System.Drawing.Size(63, 13)
+        Me.lblTargetPath.TabIndex = 1
+        Me.lblTargetPath.Text = "Target Path"
+        '
+        'lblOutputPath
+        '
+        Me.lblOutputPath.AutoSize = True
+        Me.lblOutputPath.Location = New System.Drawing.Point(12, 80)
+        Me.lblOutputPath.Name = "lblOutputPath"
+        Me.lblOutputPath.Size = New System.Drawing.Size(64, 13)
+        Me.lblOutputPath.TabIndex = 3
+        Me.lblOutputPath.Text = "Output Path"
+        '
+        'txtOutputPath
+        '
+        Me.txtOutputPath.Location = New System.Drawing.Point(12, 96)
+        Me.txtOutputPath.Name = "txtOutputPath"
+        Me.txtOutputPath.Size = New System.Drawing.Size(776, 20)
+        Me.txtOutputPath.TabIndex = 2
+        '
+        'cmdUnpack
+        '
+        Me.cmdUnpack.Location = New System.Drawing.Point(12, 122)
+        Me.cmdUnpack.Name = "cmdUnpack"
+        Me.cmdUnpack.Size = New System.Drawing.Size(75, 23)
+        Me.cmdUnpack.TabIndex = 4
+        Me.cmdUnpack.Text = "Unpack"
+        Me.cmdUnpack.UseVisualStyleBackColor = True
+        '
+        'txtLog
+        '
+        Me.txtLog.Location = New System.Drawing.Point(12, 151)
+        Me.txtLog.Multiline = True
+        Me.txtLog.Name = "txtLog"
+        Me.txtLog.ReadOnly = True
+        Me.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtLog.Size = New System.Drawing.Size(776, 317)
+        Me.txtLog.TabIndex = 5
+        '
+        'cmdSelectFile
+        '
+        Me.cmdSelectFile.Location = New System.Drawing.Point(12, 51)
+        Me.cmdSelectFile.Name = "cmdSelectFile"
+        Me.cmdSelectFile.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSelectFile.TabIndex = 6
+        Me.cmdSelectFile.Text = "File"
+        Me.cmdSelectFile.UseVisualStyleBackColor = True
+        '
+        'cmdSelectDir
+        '
+        Me.cmdSelectDir.Location = New System.Drawing.Point(93, 51)
+        Me.cmdSelectDir.Name = "cmdSelectDir"
+        Me.cmdSelectDir.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSelectDir.TabIndex = 7
+        Me.cmdSelectDir.Text = "Folder"
+        Me.cmdSelectDir.UseVisualStyleBackColor = True
+        '
+        'cmdNoFileNameUnpack
+        '
+        Me.cmdNoFileNameUnpack.Location = New System.Drawing.Point(93, 122)
+        Me.cmdNoFileNameUnpack.Name = "cmdNoFileNameUnpack"
+        Me.cmdNoFileNameUnpack.Size = New System.Drawing.Size(75, 23)
+        Me.cmdNoFileNameUnpack.TabIndex = 8
+        Me.cmdNoFileNameUnpack.Text = "No Name"
+        Me.cmdNoFileNameUnpack.UseVisualStyleBackColor = True
+        '
+        'cmdQuickScript
+        '
+        Me.cmdQuickScript.Location = New System.Drawing.Point(174, 122)
+        Me.cmdQuickScript.Name = "cmdQuickScript"
+        Me.cmdQuickScript.Size = New System.Drawing.Size(83, 23)
+        Me.cmdQuickScript.TabIndex = 9
+        Me.cmdQuickScript.Text = "Quick Action"
+        Me.cmdQuickScript.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 480)
+        Me.Controls.Add(Me.cmdQuickScript)
+        Me.Controls.Add(Me.cmdNoFileNameUnpack)
+        Me.Controls.Add(Me.cmdSelectDir)
+        Me.Controls.Add(Me.cmdSelectFile)
+        Me.Controls.Add(Me.txtLog)
+        Me.Controls.Add(Me.cmdUnpack)
+        Me.Controls.Add(Me.lblOutputPath)
+        Me.Controls.Add(Me.txtOutputPath)
+        Me.Controls.Add(Me.lblTargetPath)
+        Me.Controls.Add(Me.txtTargetPath)
+        Me.Name = "frmMain"
+        Me.Text = "SAO HF Unpack OFS3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtTargetPath As TextBox
+    Friend WithEvents lblTargetPath As Label
+    Friend WithEvents lblOutputPath As Label
+    Friend WithEvents txtOutputPath As TextBox
+    Friend WithEvents cmdUnpack As Button
+    Friend WithEvents txtLog As TextBox
+    Friend WithEvents cmdSelectFile As Button
+    Friend WithEvents cmdSelectDir As Button
+    Friend WithEvents cmdNoFileNameUnpack As Button
+    Friend WithEvents cmdQuickScript As Button
+End Class
---
+++ utils/SAOHFUnpackOSF3/SAOHFUnpackOSF3/frmMain.vb
@@ -0,0 +1,213 @@
+﻿Imports System.IO
+
+Public Class frmMain
+    Private Sub cmdSelectFile_Click(sender As Object, e As EventArgs) Handles cmdSelectFile.Click
+        Using ofd As New OpenFileDialog
+            ofd.Filter = "All files (*.*)|*.*"
+            ofd.Multiselect = False
+            If ofd.ShowDialog() = DialogResult.OK Then
+                txtTargetPath.Text = ofd.FileName
+            End If
+        End Using
+    End Sub
+
+    Private Sub cmdSelectDir_Click(sender As Object, e As EventArgs) Handles cmdSelectDir.Click
+
+    End Sub
+
+    Private Sub cmdUnpack_Click(sender As Object, e As EventArgs) Handles cmdUnpack.Click
+        If txtOutputPath.Text = "" Then
+            txtOutputPath.Text = Application.StartupPath() + "\output"
+            'MsgBox("Invalid output path")
+            'Exit Sub
+        End If
+        txtLog.Text = ""
+        addLog("Unpacking " + txtTargetPath.Text + " to " + txtOutputPath.Text + "....")
+
+        If txtTargetPath.Text.EndsWith("\") Then
+            folderUnpack(txtTargetPath.Text)
+        Else
+            unpack(txtTargetPath.Text, False)
+        End If
+    End Sub
+
+    Private Sub unpack(ByVal arcPath As String, ByVal multi As Boolean)
+        'File stream
+        Dim fs1 As FileStream = File.Open(arcPath, FileMode.Open, FileAccess.Read, FileShare.Write)
+
+        'reader
+        Dim reader As BinaryReader = New BinaryReader(fs1)
+
+        'check OFS3 magic
+        Dim magicTest(3) As Byte
+        Dim magicTest2() As Byte = {&H4F, &H46, &H53, &H33}
+        fs1.Read(magicTest, 0, 4)
+        If Not magicTest.SequenceEqual(magicTest2) Then
+            addLog("Couldn't find OFS3 magic")
+            Exit Sub
+        End If
+
+        'header stuff
+        Dim headerLength As UInt32 = reader.ReadUInt32()
+        Dim archiveType As UInt16 = reader.ReadUInt16()
+        Dim archiveConfig As UInt16 = reader.ReadUInt16()
+        Dim nonHeaderSize As UInt32 = reader.ReadUInt32()
+        Dim numFiles As UInt32 = reader.ReadUInt32()
+
+        'track offset in file name table
+        Dim fileNameTableLocation As UInt32 = nonHeaderSize + 16
+        Dim fileNameTableTracker As UInt32 = fileNameTableLocation
+        reader.BaseStream.Seek(-1, SeekOrigin.End)
+        Dim totalSize As UInt32 = reader.BaseStream.Position
+
+        For i As Integer = 0 To numFiles - 1
+            reader.BaseStream.Seek(20 + i * 8, SeekOrigin.Begin)
+            Dim fileOffset As UInt32 = reader.ReadUInt32() + 16
+            Dim fileSize As UInt32 = reader.ReadUInt32()
+            Dim fileData(fileSize - 1) As Byte
+
+            reader.BaseStream.Seek(fileOffset, SeekOrigin.Begin)
+            reader.Read(fileData, 0, fileSize)
+            reader.BaseStream.Seek(fileNameTableTracker, SeekOrigin.Begin)
+
+            'remaining fnt to byte array
+            Dim tempFileNameArray(totalSize - fileNameTableTracker) As Byte
+            reader.Read(tempFileNameArray, 0, tempFileNameArray.Length - 1)
+
+            'Find first index of null terminator
+            Dim cutIndex As UInt32 = 0
+            For j As Integer = 0 To tempFileNameArray.Length - 1
+                If tempFileNameArray(j) = &H0 Then
+                    Exit For
+                Else
+                    cutIndex += 1
+                End If
+            Next
+
+            Dim finalFileNameArray(cutIndex - 1) As Byte
+            reader.BaseStream.Seek(fileNameTableTracker, SeekOrigin.Begin)
+            reader.Read(finalFileNameArray, 0, finalFileNameArray.Length)
+            fileNameTableTracker += cutIndex + 1
+
+            Dim finalFileName As String = System.Text.Encoding.ASCII.GetString(finalFileNameArray)
+
+            If finalFileName = "" Then
+                Continue For
+            End If
+
+            File.WriteAllBytes(txtOutputPath.Text + "\" + finalFileName, fileData)
+
+            addLog("Wrote file " + finalFileName)
+        Next
+
+        If Not multi Then
+            addLog("Finished!!")
+        End If
+    End Sub
+
+    Private Sub addLog(ByVal message As String)
+        If Not txtLog.Text = "" Then
+            txtLog.Text += vbNewLine
+        End If
+        txtLog.Text += message
+    End Sub
+
+    Private Sub cmdNoFileNameUnpack_Click(sender As Object, e As EventArgs) Handles cmdNoFileNameUnpack.Click
+        If txtOutputPath.Text = "" Then
+            txtOutputPath.Text = Application.StartupPath() + "\output"
+            'MsgBox("Invalid output path")
+            'Exit Sub
+        End If
+        txtLog.Text = ""
+        addLog("Unpacking " + txtTargetPath.Text + " to " + txtOutputPath.Text + "....")
+
+        If txtTargetPath.Text.EndsWith("\") Then
+            folderNoFNUnpack(txtTargetPath.Text)
+        Else
+            noFNUnpack(txtTargetPath.Text, False)
+        End If
+
+    End Sub
+
+    Private Sub noFNUnpack(ByVal arcPath As String, ByVal multi As Boolean)
+        'File stream
+        Dim fs1 As FileStream = File.Open(arcPath, FileMode.Open, FileAccess.Read, FileShare.Write)
+
+        'reader
+        Dim reader As BinaryReader = New BinaryReader(fs1)
+
+        'check OFS3 magic
+        Dim magicTest(3) As Byte
+        Dim magicTest2() As Byte = {&H4F, &H46, &H53, &H33}
+        fs1.Read(magicTest, 0, 4)
+        If Not magicTest.SequenceEqual(magicTest2) Then
+            addLog("Couldn't find OFS3 magic")
+            Exit Sub
+        End If
+
+        'header stuff
+        Dim headerLength As UInt32 = reader.ReadUInt32()
+        Dim archiveType As UInt16 = reader.ReadUInt16()
+        Dim archiveConfig As UInt16 = reader.ReadUInt16()
+        Dim nonHeaderSize As UInt32 = reader.ReadUInt32()
+        Dim numFiles As UInt32 = reader.ReadUInt32()
+
+        Dim fileNameInt As UInt32 = 0
+
+        For i As Integer = 0 To numFiles - 1
+            reader.BaseStream.Seek(20 + i * 8, SeekOrigin.Begin)
+            Dim fileOffset As UInt32 = reader.ReadUInt32() + 16
+            Dim fileSize As UInt32 = reader.ReadUInt32()
+            Dim fileData(fileSize - 1) As Byte
+
+            reader.BaseStream.Seek(fileOffset, SeekOrigin.Begin)
+            reader.Read(fileData, 0, fileSize)
+
+            Dim baseFN As String = arcPath.Substring(arcPath.LastIndexOf("\") + 1) + "_"
+
+            Dim finalFilename As String = baseFN + fileNameInt.ToString()
+            fileNameInt += 1
+
+            File.WriteAllBytes(txtOutputPath.Text + "\" + finalFilename, fileData)
+
+            addLog("Wrote file " + finalFilename)
+        Next
+
+        If Not multi Then
+            addLog("Finished!!")
+        End If
+
+    End Sub
+
+    Private Sub folderUnpack(ByVal arcPath As String)
+        Dim di As New DirectoryInfo(arcPath)
+        Dim fiArr As FileInfo() = di.GetFiles()
+        Dim fri As FileInfo
+        For Each fri In fiArr
+            unpack(fri.FullName, True)
+        Next
+        addLog("Finished!!")
+    End Sub
+
+    Private Sub folderNoFNUnpack(ByVal arcPath As String)
+        Dim di As New DirectoryInfo(arcPath)
+        Dim fiArr As FileInfo() = di.GetFiles()
+        Dim fri As FileInfo
+        For Each fri In fiArr
+            noFNUnpack(fri.FullName, True)
+        Next
+        addLog("Finished!!")
+    End Sub
+
+    Private Sub cmdQuickScript_Click(sender As Object, e As EventArgs) Handles cmdQuickScript.Click
+        'Dim di As New DirectoryInfo(txtOutputPath.Text)
+        'Dim fiArr As FileInfo() = di.GetFiles()
+        'Dim fri As FileInfo
+        'For Each fri In fiArr
+        '    If Not fri.FullName.EndsWith("_1") Then
+        '        File.Delete(fri.FullName)
+        '    End If
+        'Next
+        addLog("Ran Quick Script!!")
+    End Sub
+End Class
